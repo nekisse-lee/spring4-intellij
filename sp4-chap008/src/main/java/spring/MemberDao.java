6ab7970f5b2fe6aed501dede7cf30968cd12bd01
nekisse <lsh891224@gmail.com>
2017-11-19 17:35:54 +0900

    count()  update() 구현 MainForMemberDao 클래스 작 count()메서드의 queryForObject() 는   결과 컬럼이  반드시 1행일때 사용
    update()메서드의  PreparedStatementCreator 클래스 사용 방법

--- sp4-chap008/src/main/java/spring/MemberDao.java
+++ sp4-chap008/src/main/java/spring/MemberDao.java
@@ -1,11 +1,14 @@
 package spring;
 
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Collection;
 import java.util.List;
 
@@ -61,36 +64,111 @@ public class MemberDao {
 		return results.isEmpty() ? null : results.get(0);
 	}
 
-	public void insert(Member member) {
-		//TODO 구현해야 함
+	public void insert(final Member member) {
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement pstmt = con.prepareStatement(
+						"insert into MEMBER (EMAIL, PASSWORD, NAME, REGDATE)" +
+								"values(?,?,?,?)",
+						new String[]{"ID"});
+				pstmt.setString(1, member.getEmail());
+				pstmt.setString(2, member.getPassword());
+				pstmt.setString(3, member.getName());
+				pstmt.setTimestamp(4,
+						new Timestamp(member.getRegisterDate().getTime()));
+				return pstmt;
+			}
+		}, keyHolder);
+		Number keyValue = keyHolder.getKey();
+		member.setId(keyValue.longValue());
+
 	}
 
 	public void update(Member member) {
-		//TODO 구현해야 함
+//		jdbcTemplate.update(
+//				"update Member set NAME = ?, PASSWORD = ?, where EMAIL = ? ",
+//				member.getName(), member.getPassword(), member.getEmail()
+//		);
+// 			NAME = ? 는 member.getName(),   PASSWORD = ? 는 member.getPassword(), EMAIL = ? 는  member.getEmail()
+//		로 쿼리의 인덱스 파라미터의 값으로 사용
+
+			//경우의 따라 PreparedStatement 의 set 메서드를 사용해서 인덱스 파라미터 값을 설정해주어야 할 때가 있다
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement pstmt = con.prepareStatement(
+						"insert into MEMBER(EMAIL, PASSWORD, NAME,REGDATE) values (?,?,?,?)");
+				return pstmt;
+			}
+		});
+
 	}
 
-	public Collection<Member> selectAll() {
-		/*List<Member> results= jdbcTemplate.query("select * from Member",
-				new RowMapper<Member>() {
-					@Override
-					public Member mapRow(ResultSet rs, int rowNum)
-							throws SQLException {
-						Member member = new Member(rs.getString("EMAIL"),
-								rs.getString("PASSWORD"),
-								rs.getString("NAME"),
-								rs.getTimestamp("REGDATE"));
-						member.setId(rs.getLong("ID"));
-						return member;
-
-					}
-				});
-		return results;*/
-
+    public List<Member> selectAll() {
+        List<Member> results = jdbcTemplate.query("select * from MEMBER",
+                new RowMapper<Member>() {
+                    @Override
+                    public Member mapRow(ResultSet rs, int rowNum)
+                            throws SQLException {
+                        Member member = new Member(rs.getString("EMAIL"),
+                                rs.getString("PASSWORD"),
+                                rs.getString("NAME"),
+                                rs.getTimestamp("REGDATE"));
+                        member.setId(rs.getLong("ID"));
+                        return member;
+                    }
+                });
+        return results;
+    }
 
 		//   중복코드 를 제거하기 위해 MemberRowMapper 클래스를 정의하고 사용
-		List<Member> results = jdbcTemplate.query("select * from Member",
-				new MemberRowMapper());
-		return results;
+//		List<Member> results = jdbcTemplate.query("select * from Member",
+//				new MemberRowMapper());
+//		return results;
+//	}
+
+	public int count(){
+//		List<Integer> results = jdbcTemplate.query(
+//				"select count(*) from MEMBER",
+//				new RowMapper<Integer>() {
+//
+//					@Override
+//					public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
+//						return rs.getInt(1);
+//					}
+//				});
+//		return results.get(0);
+
+
+		// 결과 행이 한 개의 컬럼만 가진 경우를 위한 메서드!   두번째 파라미터는 칼럼을 읽어 올 때 사용할 타입을 지정!
+		//만약 평균을 구하는 경우 Double.class 타입을 사용
+		Integer count = jdbcTemplate.queryForObject(
+				"select count(*) from MEMBER", Integer.class);
+		return count;
+
+		//실행 결과과 두개 이상의 컬럼을 가지고 있는 경우 RowMapper를 파라미터로 전달해서  값을 전달 받을 수 있다
+		//기존의 query()문과 차이점은  리턴 타입이 List가 아니라 RowMapper로 변환해주는 타입 ( 아래 코드에서는 Member)라는 점이다
+		// queryForObject()  메서드를 사용할때 주의점은 쿼리 실행 결과는 반드시!!!! 한 행이어야 한다 쿼리 실행 개수가 0이나 2 이상이면
+		// IncorrectResultSizeDataAccessException 을 발생  ,  행의 개수가 0이면 EmptyResultDataAccessExcetpion 발생
+//		Member member = jdbcTemplate.queryForObject(
+//				"select * from MEMBER where ID = ?",
+//				new RowMapper<Member>() {
+//					@Override
+//					public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+//						Member member = new Member(rs.getString("EMAIL"),
+//								rs.getString("PASSWORD"),
+//								rs.getString("NAME"),
+//								rs.getTimestamp("REGDATE"));
+//						member.setId(rs.getLong("ID"));
+//						return member;
+//					}
+//				},
+//				100);
+
+
+
 	}
 
 }
