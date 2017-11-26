8c21af1fbe046c5005fc51a99b2a5a8a434bffae
nekisse <lsh891224@gmail.com>
2017-11-26 18:16:25 +0900

    컨트롤러에서 쿠키 사용하기  P 365 loginForm.jsp에 이메일 기억하기 선택 항목 추가. LoginController의 form()메서드에서 쿠키가 존재하는 경우 폼에 전달한 커맨드 객체의 email프로퍼티를 쿠키의 값으로 설정. LoginController의 submit() 메서드에서, 이메일 기억하기 옵션을 선택한 경우 로그인 성공 후에 이메일을 담고있는 쿠키를 생성.
    쿠키 어노테이션 사용방법
    @RequestMapping(method = RequestMethod.GET)
        public String form(LoginCommand loginCommand, @CookieValue(value = "REMEMBER", required = false)Cookie rCookie) {
            if (rCookie != null) {
                loginCommand.setEmail(rCookie.getValue());
                loginCommand.setRememberEmail(true);
            }
            return "login/loginForm";
        }
    
    @CookieValue 어노테이션의 value속성으 쿠키의 이름
    지정한 이름을 가진 쿠키가 존재하지 않을 수 있다면 required속성의 값을 false 지정
    보통 false로 지정.... 기본값은 true인데 required가 true인 상태에서 지정한 이름을 가진 쿠키가 존재하지 않으면 스프링 MVC는 익셉션을 발생.
    
    실제로 쿠키를 생성하는 부분은은    submit() 메서드이다. 쿠키를 생성하려면 HttpServletResponse 타입을 파라미터로 추가해 주어야한다.

--- sp4-chap0012/src/main/java/controller/LoginController.java
+++ sp4-chap0012/src/main/java/controller/LoginController.java
@@ -2,12 +2,15 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import spring.AuthInfo;
 import spring.AuthService;
 import spring.IdPasswordNotMatchingException;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -21,12 +24,16 @@ public class LoginController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public String from(LoginCommand loginCommand) {
+    public String form(LoginCommand loginCommand, @CookieValue(value = "REMEMBER", required = false)Cookie rCookie) {
+        if (rCookie != null) {
+            loginCommand.setEmail(rCookie.getValue());
+            loginCommand.setRememberEmail(true);
+        }
         return "login/loginForm";
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String submit(LoginCommand loginCommand, Errors errors, HttpSession session) {
+    public String submit(LoginCommand loginCommand, Errors errors, HttpSession session, HttpServletResponse response) {
         new LoginCommandValidator().validate(loginCommand, errors);
         if (errors.hasErrors()) {
             return "login/loginForm";
@@ -37,6 +44,15 @@ public class LoginController {
                     loginCommand.getPassword());
             //TODO 세션에 authInfo 저장
             session.setAttribute("authInfo", authInfo);
+
+            Cookie rememberCookie = new Cookie("Remember", loginCommand.getEmail());
+            rememberCookie.setPath("/");
+            if (loginCommand.isRememberEmail()) {
+                rememberCookie.setMaxAge(60 * 60 * 24 * 30);
+            } else {
+                rememberCookie.setMaxAge(0);
+            }
+            response.addCookie(rememberCookie);
 
             return "login/loginSuccess";
         } catch (IdPasswordNotMatchingException e) {
