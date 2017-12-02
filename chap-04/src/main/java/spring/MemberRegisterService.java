f2d5dcadfdc4664d8176ed984ce6d03ac1b66a0d
nekisse <lsh891224@gmail.com>
2017-12-02 21:05:45 +0900

    chap-04 자바 빈 설정 사

---
+++ chap-04/src/main/java/spring/MemberRegisterService.java
@@ -0,0 +1,29 @@
+package spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+public class MemberRegisterService {
+	private MemberDao memberDao;
+
+
+	@Autowired
+	public MemberRegisterService(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+
+	public MemberRegisterService() {
+	}
+
+	public void regist(RegisterRequest req) {
+		Member member = memberDao.selectByEmail(req.getEmail());
+		if (member != null) {
+			throw new AlreadyExistingMemberException("dup email " + req.getEmail());
+		}
+		Member newMember = new Member(
+				req.getEmail(), req.getPassword(), req.getName(),
+				new Date());
+		memberDao.insert(newMember);
+	}
+}
---
+++ chap-05/src/main/java/main/Main.java
@@ -0,0 +1,32 @@
+package main;
+
+import config.JavaConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.MemberInfoPrinter;
+import spring.MemberRegisterService;
+import spring.RegisterRequest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext ctx =
+                new AnnotationConfigApplicationContext(JavaConfig.class);
+
+        MemberRegisterService regSvc =
+                ctx.getBean("memberRegSvc", MemberRegisterService.class);
+
+        MemberInfoPrinter infoPrinter =
+                ctx.getBean("infoPrinter", MemberInfoPrinter.class);
+
+        RegisterRequest regReq = new RegisterRequest();
+        regReq.setEmail("nekisse@nekisse.net");
+        regReq.setName("이선호");
+        regReq.setPassword("1234");
+        regReq.setConfirmPassword("1234");
+        regSvc.regist(regReq);
+
+        infoPrinter.printMemberInfo("nekisse@nekisse.net");
+
+    }
+}
