1c2f08c5e026aaf39c3bd87d167dd3b93b9d37b6
nekisse <lsh891224@gmail.com>
2017-12-04 18:39:43 +0900

    AOP 개념  공통기능
    프록시 - 핵심기능의 실행은 다른객체에  위임하고 부가적인 기능을 제공하는 객체
    		(ExetimeCalculator.class)
    대상객체 - 실제 핵심기능을 실행하는 객체
    		(ImpeCalculator.class , RecCalculator.class)

---
+++ chap-07/src/main/java/chap07/ImpeCalculator.java
@@ -0,0 +1,16 @@
+package chap07;
+
+public class ImpeCalculator implements Calculator {
+
+    public long factorial(long num) {
+        long result = 1;
+        for (int i = 1; i <= num; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
+
+
+
+
---
+++ chap-07/src/main/java/main/test.java
@@ -0,0 +1,15 @@
+package main;
+
+import chap07.ImpeCalculator;
+
+public class test {
+    public static void main(String[] args) {
+
+
+        ImpeCalculator impeCalculator = new ImpeCalculator();
+        impeCalculator.factorial(3);
+
+
+
+    }
+}
