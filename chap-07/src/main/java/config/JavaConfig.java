9e573b912b83bdd2137d607a1fdfaa0117ddae11
nekisse <lsh891224@gmail.com>
2017-12-05 16:47:53 +0900

    AOP의 기본개념  핵심 기능에 공통 기능을 삽입하는 것
    공통기능 클래스 만들기.  (Aspect)
    xml 기반
    
        <bean id="exeTimeAspect" class="aspect.ExeTimeAspect"/>
    
        <aop:config>
            <aop:aspect id="measureAspect" ref="exeTimeAspect">
                <aop:pointcut id="publicMethod"
                              expression="execution(public * chap07..*(..))"/>
                <aop:around pointcut-ref="publicMethod" method="measure"/>
            </aop:aspect>
        </aop:config>
    
    @Aspect 어노테이션은
    aspect 클래스에  공통기능과,  Pointcut을 설정해준다
    
    @Aspect 어노테이션을 인식할수 있도록
    XML설정에서  <aop:aspectj-autoproxy /> 태그를 추가해주어야한다
    
    클래스에 @Aspect 어노테이션 붙이기
    
    포인트컷 지정
        @Pointcut("execution(public * chap07..*(..))")
        private void publicTarget() {
        }
    
    @Around("publicTarget()")         포인트컷에 설정한 메소드이름 사용
    public.. 공통기능메서드
    
    @Configuration 어노테이션 이용 자바설정을 사용한다면
    @EnableAspectJAutoProxy 어노테이션을 적용해야한다.

---
+++ chap-07/src/main/java/config/JavaConfig.java
@@ -0,0 +1,24 @@
+package config;
+
+import aspect.ExeTimeAspect2;
+import chap07.Calculator;
+import chap07.RecCalculator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class JavaConfig {
+
+    @Bean
+    public ExeTimeAspect2 exeTimeAspect(){
+        return new ExeTimeAspect2();
+    }
+
+    @Bean
+    public Calculator recCal(){
+        return new RecCalculator();
+    }
+
+}
