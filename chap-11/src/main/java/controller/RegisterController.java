89260cf8ceaed888a20ce63b88598b438bf1e58c
nekisse <lsh891224@gmail.com>
2017-12-11 16:29:50 +0900

    redirect:/경로
    커맨드객체 사용.
    @RequestMapping 어노테이션이 적용된 메서드의 파라미터로 사용
    @RequestMapping(value = "/register/step3", method = RequestMethod.POST)
        public String handleStep3(RegisterRequest regReq) {
            try {
                memberRegisterService.regist(regReq);
                return "register/step3";
            } catch (AlreadyExistingMemberException ex) {
                return "register/step2";
            }
        }

--- chap-11/src/main/java/controller/RegisterController.java
+++ chap-11/src/main/java/controller/RegisterController.java
@@ -5,9 +5,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import spring.AlreadyExistingMemberException;
+import spring.MemberRegisterService;
+import spring.RegisterRequest;
 
 @Controller
 public class RegisterController {
+    private MemberRegisterService memberRegisterService;
+
+    public void setMemberRegisterService(MemberRegisterService memberRegisterService) {
+        this.memberRegisterService = memberRegisterService;
+    }
 
     @RequestMapping("/register/step1")
     public String handleStep1() {
@@ -27,5 +35,15 @@ public class RegisterController {
     @RequestMapping(value = "/register/step2", method = RequestMethod.GET)
     public String handleStep2Get() {
         return "redirect:/register/step1";
+    }
+
+    @RequestMapping(value = "/register/step3", method = RequestMethod.POST)
+    public String handleStep3(RegisterRequest regReq) {
+        try {
+            memberRegisterService.regist(regReq);
+            return "register/step3";
+        } catch (AlreadyExistingMemberException ex) {
+            return "register/step2";
+        }
     }
 }
