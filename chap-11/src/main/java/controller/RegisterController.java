948c439f70b9812e98df2a07ddda707398bfdc9c
nekisse <lsh891224@gmail.com>
2017-12-11 15:57:06 +0900

    요청파라미터 접근 방법 1. HttpServletRequest을 직접이용한다 public String handleStep2(HttpServletRequest request){ String agreeParam= request.getParameter("agree");
    2. @RequestParam을 이용한다.
      value  =   타입은 String HTTP요청 파라미터의 이름,
    required = 타입은 boolean 필수여부 지정, 기본값은 true  이 값이 true이면서 해당요청 파라미터에 값이 없으면 스프링 MVC는 익셉션 발생시킴
    defaultValue  = 타입은 String  요청 파라미터가 값이 없을때 사용할 문자열 값을 지정,  기본값은 없다.
    @RequestParam(value = "agree", defaultValue = "false") Boolean agree) {

--- chap-11/src/main/java/controller/RegisterController.java
+++ chap-11/src/main/java/controller/RegisterController.java
@@ -3,12 +3,29 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RegisterController {
 
     @RequestMapping("/register/step1")
-    public String handleStep1(){
+    public String handleStep1() {
         return "register/step1";
+    }
+
+
+    @RequestMapping(value = "/register/step2", method = RequestMethod.POST)
+    public String handleStep2(
+            @RequestParam(value = "agree", defaultValue = "false") Boolean agree) {
+        if (!agree) {
+            return "register/step1";
+        }
+        return "register/step2";
+    }
+
+    @RequestMapping(value = "/register/step2", method = RequestMethod.GET)
+    public String handleStep2Get() {
+        return "redirect:/register/step1";
     }
 }
